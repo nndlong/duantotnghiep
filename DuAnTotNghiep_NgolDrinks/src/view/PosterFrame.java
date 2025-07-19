9bdb565455229fa7fba1671259762676859fb375
nndlong <longnndth06881@gmail.com>
2025-07-19 22:01:23 +0700

    Fix PosterFrame, delete img trash

--- DuAnTotNghiep_NgolDrinks/src/view/PosterFrame.java
+++ DuAnTotNghiep_NgolDrinks/src/view/PosterFrame.java
@@ -7,20 +7,21 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-    
+
 public class PosterFrame extends javax.swing.JFrame {
-private JLabel lblPoster, lblClick;
+
+    private JLabel lblPoster, lblClick;
     private String[] imagePaths = {
         "/images/1.png",
         "/images/2.png",
         "/images/3.png"
     };
-       
+
     private int currentIndex = 0;
     private Timer imageTimer;
-    
+
     public PosterFrame() {
-                setTitle("Poster NgolDrinks");
+        setTitle("Poster NgolDrinks");
         setSize(800, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -58,15 +59,19 @@ private JLabel lblPoster, lblClick;
         });
     }
 
- private void updateImage() {
-    try {
-        ImageIcon icon = new ImageIcon(getClass().getResource(imagePaths[currentIndex]));
+private void updateImage() {
+    String path = imagePaths[currentIndex];
+    java.net.URL imgUrl = getClass().getResource(path);
+    if (imgUrl == null) {
+        System.out.println("❌ Không tìm thấy ảnh tại: " + path);
+    } else {
+        System.out.println("✅ Đã load ảnh: " + path);
+        ImageIcon icon = new ImageIcon(imgUrl);
         Image img = icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
         lblPoster.setIcon(new ImageIcon(img));
-    } catch (Exception e) {
-        System.out.println("Không tìm thấy ảnh: " + imagePaths[currentIndex]);
     }
 }
+
 
 
     /**
@@ -98,7 +103,7 @@ private JLabel lblPoster, lblClick;
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
